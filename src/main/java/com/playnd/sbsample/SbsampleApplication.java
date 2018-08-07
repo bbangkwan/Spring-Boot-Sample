e61e1419b77a9ffba9a90aed0dc8b44290f1724e
byeongkwanbae <bbangkwan@gmail.com>
2018-08-07 13:21:26 +0900

    yml 적용

--- src/main/java/com/playnd/sbsample/SbsampleApplication.java
+++ src/main/java/com/playnd/sbsample/SbsampleApplication.java
@@ -1,7 +1,9 @@
 package com.playnd.sbsample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SbsampleApplication {
--- src/main/java/com/playnd/sbsample/controller/MainController.java
+++ src/main/java/com/playnd/sbsample/controller/MainController.java
@@ -1,9 +1,13 @@
 package com.playnd.sbsample.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sun.applet.Main;
 
 /**
  * Created by Bae B K on 2018. 8. 6.
@@ -12,9 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class MainController {
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+    
+    @Value("${spring.testValue}") private String testValue;
     
     @RequestMapping(value = "/init", method = RequestMethod.GET)
     public String test(){
+        log.info("current env : "+testValue);
+        
         return "Hi, Spring Boot Project Init!!!";
     }
 }
