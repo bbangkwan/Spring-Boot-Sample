02a38f6837d12491cec7cf2b4bbb1495806d9669
byeongkwanbae <bbangkwan@gmail.com>
2018-08-27 18:38:21 +0900

    controlleradvice 적용 해볼예정

---
+++ src/main/java/com/playnd/sbsample/controller/RestfulApiControllerAdvice.java
@@ -0,0 +1,10 @@
+package com.playnd.sbsample.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Created by Bae B K on 2018. 8. 27.
+ */
+@RestControllerAdvice
+public class RestfulApiControllerAdvice {
+}
